0ba6e19334c52cd7cee315344b756d0ad01343fe
Chris Maddox <tyre77@gmail.com>
2015-05-16 22:34:06 -0700

    better README, add LICENSE

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# sorted_set
-Sorted Set library for Elixir
+# SortedSet
+A sorted set library for Elixir. Implements the
+[Set](http://elixir-lang.org/docs/v1.0/elixir/Set.html) protocol.
+
+## Installation
+
+Add the following to `deps` section of your `mix.exs`:
+  `{:sorted_set, "~> 0.1"}`
+
+and then `mix deps.get`. That's it!
+
+Generate the documentations with `mix docs`.
+
+
--- mix.exs
+++ mix.exs
@@ -3,27 +3,16 @@ defmodule SortedSet.Mixfile do
 
   def project do
     [app: :sorted_set,
-     version: "0.0.1",
+     version: "0.1",
+     source_url: "https://github.com/SenecaSystems/sorted_set",
      elixir: "~> 1.0",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:earmark, "~> 0.1", only: :dev},
